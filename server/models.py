d51e6b8f7ec3c4194b3172a4a95c4ddd61e6ee87
Chevy <vallchevy@gmail.com>
2024-08-07 14:01:38 -0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -6,4 +6,8 @@ db = SQLAlchemy()
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    image = db.Column(db.String(200))
+    price = db.Column(db.Float)
+
     id = db.Column(db.Integer, primary_key=True)
